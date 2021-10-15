d35d354b71caaff9b58319d721458ab0d2e4fbed
Tomasz Czerwinski <92301036+Tomxelse@users.noreply.github.com>
2021-10-15 13:30:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,64 @@
-const HOURHAND = document.querySelector("#hour");
-const MINUTEHAND = document.querySelector("#minute");
-const SECONDHAND = document.querySelector("#second");
-
-var date = new Date();
-console.log(date);
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
-
-let hrPosition = (hr*360/12)+(min*(360/60)/12);
-let minPosition = (min*360/60)+(sec*(360/60)/60);
-let secPosition = sec*360/60;
-
-function runTheClock() {
-
-    hrPosition = hrPosition+(3/360);
-    minPosition = minPosition+(6/60);
-    secPosition = secPosition+6;
-
-    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
-
+function cloneTicks() {
+	for (var i = 1; i <= 12; i++) {
+	var el = document.querySelector(".fiveminutes");
+	var clone = el.cloneNode(true);
+	clone.setAttribute('class', `fiveminutes f${i}`);
+	var app = document.getElementById("clockface").appendChild(clone)
+	var el2 = document.querySelector(`.f${i}`);
+	el2.style.transform = `rotate(${30 * i}deg)`;
+	}
+	
+		for (var i = 1; i <= 60; i++) {
+	var el = document.querySelector(".minutes");
+	var clone = el.cloneNode(true);
+	clone.setAttribute('class', `minutes m${i}`);
+	var app = document.getElementById("clockface").appendChild(clone)
+	var el2 = document.querySelector(`.m${i}`);
+	el2.style.transform = `rotate(${6 * i}deg)`;
+	}
 }
 
-var interval = setInterval(runTheClock, 1000);
+var synth = window.speechSynthesis;
+
+const sechand = document.querySelector('.sec')
+const minhand = document.querySelector('.min')
+const hourhand = document.querySelector('.hour')
+
+const speaker = document.getElementById('speaker');
+
+var sec, min, hour;
+
+function setTime() {
+	const now = new Date();
+	
+	sec = now.getSeconds();
+	const secdeg = ((sec / 60) * 360);
+	sechand.style.transform = `rotate(${secdeg}deg)`;
+	
+	min = now.getMinutes();
+	const mindeg = ((min / 60) * 360);
+	minhand.style.transform = `rotate(${mindeg}deg)`;
+	
+	hour = now.getHours();
+	const hourdeg = ((hour + min/60) / 12 * 360);
+	hourhand.style.transform = `rotate(${hourdeg}deg)`;
+}
+
+cloneTicks();	
+setInterval(setTime, 1000);
+
+speaker.onclick = function(e) {
+	e.preventDefault;
+
+	if (min === 0) {
+	min = hour;
+	hour = 'Even';
+	}
+	if (min < 10 && min > 1) {
+	min = 'o' + min;
+	}
+
+	var utterThis = new SpeechSynthesisUtterance(`The time is ${hour} ${min}`);
+	synth.speak(utterThis);
+};
+
